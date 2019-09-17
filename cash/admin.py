bcdfd4a4c9803292e3774f34da4d23c8e616ea0d
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-09-17 22:59:04 +0700

    Add saving model

--- cash/admin.py
+++ cash/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from cash.models import Activity
+from cash.models import Activity, Saving
 
 admin.site.register(Activity)
+admin.site.register(Saving)
--- cash/models.py
+++ cash/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 
 
+class Saving(models.Model):
+    name = models.CharField(max_length=128)
+    balance = MoneyField(decimal_places=2, max_digits=17, default_currency='IDR')
+
+    def __str__(self):
+        return self.name
+
+
 class Activity(models.Model):
     ACTIVITY_TYPE_CHOICES = (
         ('in', 'Income'),
@@ -10,6 +18,7 @@ class Activity(models.Model):
     name = models.CharField(max_length=255)
     value = MoneyField(decimal_places=2, max_digits=15, default_currency='IDR')
     type = models.CharField(choices=ACTIVITY_TYPE_CHOICES, max_length=2, default='ex')
+    impacted_saving = models.ForeignKey(Saving, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
