e28cbb14e65d9f93f23fc294cd32b9f246ad838e
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-09-18 23:10:57 +0700

    Update models

--- cash/models.py
+++ cash/models.py
@@ -1,8 +1,12 @@
+import uuid
+
 from django.db import models
+from django.utils.timezone import now
 from djmoney.models.fields import MoneyField
 
 
 class Saving(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
     balance = MoneyField(decimal_places=2, max_digits=17, default_currency='IDR')
 
@@ -15,10 +19,13 @@ class Activity(models.Model):
         ('in', 'Income'),
         ('ex', 'Expense'),
     )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    timestamp = models.DateTimeField(default=now)
     name = models.CharField(max_length=255)
     value = MoneyField(decimal_places=2, max_digits=15, default_currency='IDR')
     type = models.CharField(choices=ACTIVITY_TYPE_CHOICES, max_length=2, default='ex')
     impacted_saving = models.ForeignKey(Saving, on_delete=models.CASCADE, null=True, blank=True)
+    is_monthly = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
