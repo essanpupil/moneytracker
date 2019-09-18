3a705da63030d861656c2e862f16caa0ae2da0f9
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-09-18 07:02:12 +0700

    Display all fields in admin site

--- cash/admin.py
+++ cash/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from cash.models import Activity, Saving
 
-admin.site.register(Activity)
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Activity._meta.get_fields()]
+
+
+admin.site.register(Activity, ActivityAdmin)
+
 admin.site.register(Saving)
