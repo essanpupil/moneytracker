c0f20a4f54b6ae46829a7a8fbb9f77da0e0c809e
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-09-20 08:16:20 +0700

    Display new cash activity page

--- cash/urls.py
+++ cash/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cash.views import MainView
+from cash.views import MainView, NewActivity
 
 app_name = 'cash'
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
+    path('new', NewActivity.as_view(), name='new'),
 ]
--- cash/views.py
+++ cash/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from cash.models import Activity
 
@@ -6,3 +7,10 @@ from cash.models import Activity
 class MainView(ListView):
     model = Activity
     template_name = 'cash/index.html'
+
+
+class NewActivity(CreateView):
+    success_url = reverse_lazy('cash:main')
+    model = Activity
+    template_name = 'cash/new_activity.html'
+    fields = '__all__'
